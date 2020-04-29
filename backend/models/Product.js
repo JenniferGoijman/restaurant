753c24912ac247b6a7379a66fe89b67b7677c2e7
jguedez92 <jguedez92@gmail.com>
2020-04-29 22:45:36 +0200

    created  models - Product.js with model attributes

---
+++ backend/models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Integer,
+        required: true
+    },
+    UserId: {
+        type: ObjectId,
+        ref: 'User'
+    },
+    category: [ObjectId],
+    subCategory: [ObjectId]
+}, {
+    timestamps: true
+});
+
+const Product = mongoose.model('Peoduct', ProductSchema);
+
+module.exports = Product;
