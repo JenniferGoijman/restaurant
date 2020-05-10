65e1fee01658f851cac6c4ee6e8a1c6205f5ad2d
jguedez92 <jguedez92@gmail.com>
2020-05-10 17:25:31 +0200

    changes in cors of index

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
-module.exports={
-    jwt_auth_secret:'jgjgjgjgjgjggj234324jgjgjg3',
-    mongo_atlas:'mongodb+srv://RestaurantJG:IkRmT7yxUZE7XpP4@geeks-project-qde50.mongodb.net/test?retryWrites=true&w=majority'
+module.exports = {
+    jwt_auth_secret: 'jgjgjgjgjgjggj234324jgjgjg3',
+    mongo_atlas: 'mongodb+srv://RestaurantJG:IkRmT7yxUZE7XpP4@geeks-project-qde50.mongodb.net/test?retryWrites=true&w=majority'
 }
--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,19 @@ const usersRouter = require('./routes/users')
 const productsRouter = require('./routes/products')
 const categoriesRouter = require('./routes/categories')
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-API-KEY, Access-Control-Allow-Request-Method");
-//     res.header("Access-Control-Allow-Methods", "POST","GET","PUT","OPTIONS","DELETE");
-//     next();
-//   });
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-API-KEY, Access-Control-Allow-Request-Method");
+    res.header("Access-Control-Allow-Methods", "POST", "GET", "PUT", "OPTIONS", "DELETE");
+    next();
+});
+//app.use(cors());
 app.use(morgan('dev'))
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/users',usersRouter)
-app.use('/products',productsRouter)
-app.use('/categories',categoriesRouter)
+app.use('/users', usersRouter)
+app.use('/products', productsRouter)
+app.use('/categories', categoriesRouter)
 
 app.listen(PORT, () => console.log('server running on port: ' + PORT));
